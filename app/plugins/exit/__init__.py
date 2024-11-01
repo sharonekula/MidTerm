9a9c4b12117d3b8943260563af76a3ccd42cca76
sharonekula <se376@njit.edu>
2024-11-01 05:37:01 -0400

    logging changes

--- app/plugins/exit/__init__.py
+++ app/plugins/exit/__init__.py
@@ -1,7 +1,9 @@
+import logging
 from app.commands import Command
 from data.history import HistoryManager
 
 class ExitCommand(Command):
 
     def execute(self):
+        logging.info("System Exit Raised.")
         raise SystemExit
--- app/plugins/multiply/__init__.py
+++ app/plugins/multiply/__init__.py
@@ -1,3 +1,4 @@
+import logging
 from app.commands import Command
 from data.history import HistoryManager
 
@@ -5,7 +6,9 @@ history_manager = HistoryManager()
 class MultiplyCommand(Command):
 
     def execute(self,x , y, history_manager):
+        logging.info(f"Performing Multiplication between {x} and {y}")
         result = x * y
+        logging.info(f"Adding to history: Multiplication, {x}, {y}, {result}")
         history_manager.add_to_history("Multiplication", x, y, result)
         return result
 
